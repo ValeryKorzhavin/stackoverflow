b54b9d251f530ce1db20d77cd219594ede55e784
Valery Korzhavin <white.and.bushy@gmail.com>
2019-12-20 18:11:21 +0300

    add account controller

--- src/main/java/ru/valerykorzh/springdemo/controller/AccountController.java
+++ src/main/java/ru/valerykorzh/springdemo/controller/AccountController.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import ru.valerykorzh.springdemo.domain.Account;
 import ru.valerykorzh.springdemo.dto.mapper.AccountMapper;
 import ru.valerykorzh.springdemo.service.AccountService;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -18,8 +22,11 @@ public class AccountController {
     @GetMapping("/accounts")
     public String findAll(Model model) {
 
+        List<Account> accounts = accountService.findAll();
 
-        return "index";
+        model.addAttribute("accounts", accounts);
+
+        return "account/list";
     }
 
 }
